fe477bff37479cab827097a5333f5911b1fe1cf8
khenidenish <121486262+khenidenish@users.noreply.github.com>
2023-01-02 13:26:01 +0530

    Update HelloWorldController.java

--- src/main/java/com/example/helloworld/controller/HelloWorldController.java
+++ src/main/java/com/example/helloworld/controller/HelloWorldController.java
@@ -8,6 +8,6 @@ public class HelloWorldController {
 
     @GetMapping("/hello")
     public String sendGreetings() {
-        return "Hello, World! testing out things";
+        return "Hello, World! let's test out 2 days images";
     }
 }
